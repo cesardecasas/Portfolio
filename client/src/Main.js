22220588eb0725e1249cbce35f1e2e7f674212e9
Cesar De Casas <cesar.decasasgarcia@gmail.com>
2022-01-13 16:40:14 -0600

    updated experience

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Footer from './components/Footer'
 import PresentationCard from './components/PresentationCard'
- import technologies from './technologies.json'
+import technologies from './technologies.json'
 import Grid from './components/Grid'
 import Projects from './projects.json'
 import ProjectCard from './components/ProjectCard'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Navigation from './components/Nav'
+import experience from './experience.json'
+import ExperienceCard from './components/ExperienceCard'
 
 const Main =()=>{
 
@@ -32,11 +34,8 @@ const Main =()=>{
 
     return (
         <main style={{backgroundColor:color}}  >
-            {/* 
-            <h2 style={{margin:'4%', fontSize:'280%', color:'white'}}>Experience</h2>
-            <section className='grid' style={{width:'80%', marginLeft:'13%'}}>
-                {experience.arr.map((exp, i)=><ExperienceCard exp={exp} key={i} />)}
-            </section> */}
+            
+            
             <Navigation  set={setDarkMode} mode={darkMode}/>
             <PresentationCard color={fontColor}/>
                 <Row id='tech' style={{width:'98%', marginLeft:'1%', marginTop:'4%'}} xs={1} sm={1} md={2} lg={3}>
@@ -50,6 +49,12 @@ const Main =()=>{
                         <Grid color={color} fontColor={fontColor}  name='Tools' arr={technologies.Tools} />
                     </Col>
                 </Row>
+
+            <h2 style={{margin:'4%', fontSize:'280%', color:`${fontColor}`}}>Experience</h2>
+            <Row id='experience' xs={1} sm={2} md={3} lg={3} xl={4} style={{width:'80%', marginLeft:'10%'}}>
+                {experience.arr.reverse().map((exp, i)=><Col><ExperienceCard exp={exp} key={i} /></Col>)}
+            </Row>
+            <h2 style={{margin:'4%', fontSize:'280%', color:`${fontColor}`}}>Projects</h2>
             <Row id='projects' xs={1} sm={2} md={3} lg={3} xl={4} style={{width:'80%', marginLeft:'10%'}}>
                 {Projects.arr.map((project, index)=><Col key={index}><ProjectCard deploy={project?.deploy}  title={project.tittle} text={project.description} link={project.link}/></Col>)}
             </Row>
--- client/src/components/ExperienceCard.js
+++ client/src/components/ExperienceCard.js
@@ -6,18 +6,26 @@ const ExperienceCard =(props)=>{
     const {exp}= props
 
     return(
-        <div className='expCard'>
-            <Card border="primary" style={{width: '80%', height:'98%', marginTop:'2%', marginLeft:'2%'}}>
-                <Card.Header style={{fontSize:'150%'}}>{exp.company}</Card.Header>
-                    <Card.Body>
-                    <Card.Title style={{fontSize:'150%'}}>{exp.title}</Card.Title>
-                    <Card.Text className='text' style={{ height:"180px",fontSize:'120%', overflow:'scroll', textAlign:'left'}}>
-                        {exp.description}
-                    </Card.Text>
-                    <Card.Text>{exp.start} to {exp.end} </Card.Text>
+        <Card style={{ width: '95%', height:'98%', marginTop:'4%', boxShadow: '12px 12px 2px 1px rgba(0, 0, 255, .2)'  }} className='projectCard' >
+                <Card.Body>
+                    <Card.Title >{exp.title}</Card.Title>
+                    <Card.Subtitle>{exp.company}</Card.Subtitle>
+                    <Card.Text style={{textAlign:'left', fontSize:'110%'}} >{exp.description}</Card.Text>
                 </Card.Body>
+                <Card.Footer >             
+                    <Card.Text>{exp.start} to {exp.end} </Card.Text>
+                </Card.Footer>
             </Card>
-        </div>
+            // <Card border="primary" style={{width: '80%', height:'98%', marginTop:'2%', marginLeft:'2%'}}>
+            //     <Card.Header style={{fontSize:'150%'}}>{exp.company}</Card.Header>
+            //         <Card.Body>
+            //         <Card.Title style={{fontSize:'150%'}}>{exp.title}</Card.Title>
+            //         <Card.Text className='text' style={{ height:"180px",fontSize:'120%', overflow:'scroll', textAlign:'left'}}>
+            //             {exp.description}
+            //         </Card.Text>
+            //         <Card.Text>{exp.start} to {exp.end} </Card.Text>
+            //     </Card.Body>
+            // </Card>
     )
 }
 
